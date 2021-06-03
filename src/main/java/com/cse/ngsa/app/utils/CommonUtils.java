b60b337b39f843422610e5f6e9e2c8f0c9f27cd0
Joaquin Rodriguez <rjoaquin@microsoft.com>
2021-06-03 10:42:11 -0500

    Added X-Load-Feedback header for bursting

--- src/main/java/com/cse/ngsa/app/utils/CommonUtils.java
+++ src/main/java/com/cse/ngsa/app/utils/CommonUtils.java
@@ -116,8 +116,6 @@ public class CommonUtils {
     System.out.println("\r\nUsage:\r\n"
         + "   mvn clean spring-boot:run \r\n "
         + "\t-Dspring-boot.run.arguments=\" --help \r\n"
-        + "\t\t--auth-type=<CLI|MI|VS>\r\n"
-        + "\t\t--keyvault-name=<keyVaultName>\r\n"
         + "\t\t--dry-run\r\n"
         + "\t\t--log-level=<trace|info|warn|error|fatal>\"");
   }
