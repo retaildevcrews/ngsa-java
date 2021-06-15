85722669f251fb5c06b9353b64d3b57ee2e36fe2
Kushal Azim Ekram <mekram@microsoft.com>
2021-06-15 16:00:32 -0500

    Added JSON Request Log (#9)
    * JSON Logging with new appender and logger
    
    * Fixed formatting warnings
    
    * Added Initial Category and mode function. NEEDS to be refactored.
    
    * Fixed linting issues
    
    * Removed commented codes.

--- src/main/java/com/cse/ngsa/app/middleware/RequestLogger.java
+++ src/main/java/com/cse/ngsa/app/middleware/RequestLogger.java
@@ -1,10 +1,13 @@
 package com.cse.ngsa.app.middleware;
 
+import com.cse.ngsa.app.utils.QueryUtils;
 import java.net.InetSocketAddress;
-import java.text.MessageFormat;
+import java.time.Instant;
+import java.util.Arrays;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -34,24 +37,45 @@ public class RequestLogger implements WebFilter {
       
     // process next handler
     return webFilterChain.filter(serverWebExchange).doFinally(signalType -> {
-      // compute request duration and get status code
-      long duration = System.currentTimeMillis() - startTime;
       int statusCode = serverWebExchange.getResponse().getStatusCode().value();
       
       // don't log favicon.ico 404s
       if (pathQueryString.startsWith("/favicon.ico")) {
         return;
       }
-
+      
       // don't log if log level >= warn but response code < 400
       if (logger.getLevel().isMoreSpecificThan(Level.WARN) && statusCode < 400) {
         return;
       }
 
+      JSONObject logData = new JSONObject();
+      var currentRequest = serverWebExchange.getRequest();
+      String userAgent = currentRequest.getHeaders()
+          .getOrDefault("User-Agent", Arrays.asList("")).get(0);
+      // compute request duration and get status code
+      long duration = System.currentTimeMillis() - startTime;
+      logData.put("Date", Instant.now().toString());
+      logData.put("LogName", "Ngsa.RequestLog");
+      logData.put("StatusCode", statusCode);
+      logData.put("TTFB", duration); // Essentially ttfb in Java's case is the same as duraion
+      logData.put("Duration", duration);
+      logData.put("Verb", currentRequest.getMethod().toString());
+      logData.put("Path", pathQueryString);
+      logData.put("Host", currentRequest.getHeaders().getHost().toString());
+      logData.put("ClientIP", requestAddress);
+      logData.put("UserAgent", userAgent);
+      logData.put("CVector", "PLACEHOLDER-CV-VALUE");
+      logData.put("CVectorBase", "PLACEHOLDER-CV-BASE-VALUE");
+      String[] categoryAndMode = QueryUtils.getCategoryAndMode(serverWebExchange.getRequest());
+      logData.put("Category", categoryAndMode[0]);
+      logData.put("SubCategory", categoryAndMode[1]);
+      logData.put("Mode", categoryAndMode[2]);
+      logData.put("Zone", "dev"); // TODO: Update all props below
+      logData.put("Region", "dev");
+      logData.put("CosmosName", "in-memory");
       // log results to console
-      logger.info(MessageFormat.format(
-          "{0}\t{1}\t{2}\t{3}", 
-          statusCode, duration, requestAddress, pathQueryString));
+      logger.info(logData.toString());
     });
   }
 
