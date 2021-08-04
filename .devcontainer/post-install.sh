e54a81d1ed6e4a37caacb3c89ccc074c916a713b
Bart Robertson <bartr@microsoft.com>
2021-08-04 22:37:10 +0000

    updated

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -4,14 +4,10 @@
 mkdir -p .vscode && cp docs/vscode-template/* .vscode
 
 # set auth type
-export PATH="$PATH:~/.dotnet/tools"
 export AUTH_TYPE=CLI
-
-# install WebV global tool
-dotnet tool install -g webvalidate
 
 # update .bashrc
 echo "" >> ~/.bashrc
-echo 'export PATH="$PATH:~/.dotnet/tools"' >> ~/.bashrc
+#echo 'export PATH="$PATH:~/.dotnet/tools"' >> ~/.bashrc
 echo "export AUTH_TYPE=CLI" >> ~/.bashrc
 
