4fd6ec4ecad5a2610bd728e497a12b23c984ab24
Kushal Azim Ekram <mekram@microsoft.com>
2021-09-17 15:34:45 -0500

    Swagger Swag with URLPrefix (#68)
    * --url-prefix arg to set the url prefix.
    Added `/` and `.` route to /index.html.
    Added ResrouceTransform object to transform index.html and swagger.json
    
    * Added new line at EOF
    
    * Removed TODO and println.

--- ngsa/src/main/java/com/cse/ngsa/app/config/WebfluxConfig.java
+++ ngsa/src/main/java/com/cse/ngsa/app/config/WebfluxConfig.java
@@ -1,5 +1,7 @@
 package com.cse.ngsa.app.config;
 
+import com.cse.ngsa.app.utils.UrlPrefixTransformer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.config.ResourceHandlerRegistry;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
@@ -7,14 +9,24 @@ import org.springframework.web.reactive.config.WebFluxConfigurer;
 @Configuration
 public class WebfluxConfig implements WebFluxConfigurer {
 
+  /**
+   * This transformer will replace all {{url-prefix}} token in index.html and swagger.json
+   */
+  @Autowired
+  UrlPrefixTransformer transformer;
+
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry.addResourceHandler("/webjars/**")
         .addResourceLocations("classpath:/META-INF/resources/webjars/");
     registry.addResourceHandler("/index.html**")
-        .addResourceLocations("classpath:/static/");
+        .addResourceLocations("classpath:/static/")
+        .resourceChain(false)
+        .addTransformer(transformer); // Adding urlPrefix transformer
     registry.addResourceHandler("/swagger.json**")
-        .addResourceLocations("classpath:/static/");
+        .addResourceLocations("classpath:/static/")
+        .resourceChain(false)
+        .addTransformer(transformer); // Adding urlPrefix transformer
 
   }
 }
---
+++ ngsa/src/main/java/com/cse/ngsa/app/utils/UrlPrefixTransformer.java
@@ -0,0 +1,43 @@
+package com.cse.ngsa.app.utils;
+
+import java.nio.charset.Charset;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.reactive.resource.ResourceTransformer;
+import org.springframework.web.reactive.resource.ResourceTransformerChain;
+import org.springframework.web.reactive.resource.TransformedResource;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * Reads the resource content and replaces all {{url-prefix}} with specified
+ * url-prefix url-prefix can be specified in cli as --url-prefix=VALUE or in
+ * application.properties
+ */
+@Component
+public class UrlPrefixTransformer implements ResourceTransformer {
+
+  @Value("${url-prefix:}")
+  private String urlPrefix;
+
+  @Value("${url-prefix-value:{{url-prefix}}}")
+  private String urlPrefixValue;
+
+  @Override
+  public Mono<Resource> transform(ServerWebExchange exchange, Resource resource,
+        ResourceTransformerChain transformerChain) {
+
+    String rsrcStr;
+    try {
+      rsrcStr = StreamUtils.copyToString(resource.getInputStream(), Charset.defaultCharset());
+      rsrcStr = rsrcStr.replace(urlPrefixValue, urlPrefix);
+    } catch (Exception e) {
+      e.printStackTrace();
+      // Mostly for IOException
+      rsrcStr = "";
+    }
+    return Mono.just(new TransformedResource(resource, rsrcStr.getBytes()));
+  }
+}
