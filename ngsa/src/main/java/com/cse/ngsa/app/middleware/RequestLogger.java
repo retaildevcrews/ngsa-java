7649c83aaa8e05bb5f25835a0ad5f5dea01a6ae4
Alfredo Hernandez <52010147+alfredoihernandez@users.noreply.github.com>
2022-04-07 15:31:58 +0000

    remove correlationvector references in ngsa

--- ngsa/src/main/java/com/cse/ngsa/app/middleware/RequestLogger.java
+++ ngsa/src/main/java/com/cse/ngsa/app/middleware/RequestLogger.java
@@ -2,10 +2,8 @@ package com.cse.ngsa.app.middleware;
 
 import com.cse.ngsa.app.models.NgsaConfig;
 import com.cse.ngsa.app.services.configuration.IConfigurationService;
-import com.cse.ngsa.app.utils.CorrelationVectorExtensions;
 import com.cse.ngsa.app.utils.CpuMonitor;
 import com.cse.ngsa.app.utils.QueryUtils;
-import com.microsoft.correlationvector.CorrelationVector;
 import io.micrometer.core.instrument.DistributionSummary;
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
@@ -100,19 +98,6 @@ public class RequestLogger implements WebFilter {
       logData.put("Host", host == null ? "" : host.toString());
       logData.put("ClientIP", requestAddress);
       logData.put("UserAgent", userAgent);
-
-      // In general, no need to check if serverWebExchabge have MS-CV attribute
-      // But a good practice to check for it anyway
-      CorrelationVector cv;
-      if (serverWebExchange.getAttributes().containsKey(CorrelationVector.HEADER_NAME)) {
-        cv = (CorrelationVector) serverWebExchange.getAttribute(CorrelationVector.HEADER_NAME);
-      } else {
-        // This should never happen!
-        LogManager.getRootLogger().error("MS-CV Attribute not found. Creating a new one");
-        cv = CorrelationVectorExtensions.extend(serverWebExchange);
-      }
-      logData.put("CVector", cv.getValue());
-      logData.put("CVectorBase", cv.getBaseVector());
 
       // Get category and mode from Request
       String[] categoryAndMode = QueryUtils.getCategoryAndMode(serverWebExchange.getRequest());
