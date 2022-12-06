0384c23255b7071242e06989d962fb63072f1a35
Travis <66956775+TravisFallsMSFT@users.noreply.github.com>
2022-12-06 23:42:07 +0000

    updated for SDKman

--- .devcontainer/installJava.sh
+++ .devcontainer/installJava.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-WGET https://github.com/devcontainers/features/blob/main/src/java/devcontainer-feature.json
-WGET https://github.com/devcontainers/features/blob/main/src/java/install.sh
-WGET https://github.com/devcontainers/features/blob/main/src/java/wrapper.sh
+
+wget https://github.com/devcontainers/features/blob/main/src/java/devcontainer-feature.json
+wget https://github.com/devcontainers/features/blob/main/src/java/install.sh
+wget https://github.com/devcontainers/features/blob/main/src/java/wrapper.sh
 
 chmod +x install.sh
 chmod +x wrapper.sh
-
