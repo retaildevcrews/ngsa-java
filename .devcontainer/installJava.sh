dcff8e0d1a1ad670c6b8155951f7dce2198786f2
Travis <66956775+TravisFallsMSFT@users.noreply.github.com>
2022-12-12 17:44:34 +0000

    Deleting unneeded file, and cleaning up tech debt.

--- .devcontainer/installJava.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-wget https://github.com/devcontainers/features/blob/main/src/java/devcontainer-feature.json
-wget https://github.com/devcontainers/features/blob/main/src/java/install.sh
-wget https://github.com/devcontainers/features/blob/main/src/java/wrapper.sh
-
-chmod +x install.sh
-chmod +x wrapper.sh
